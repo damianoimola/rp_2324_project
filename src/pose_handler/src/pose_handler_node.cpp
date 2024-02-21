a6a0f551e8cca2d4a3924da13bbd0b51c65b07b9
damiano imola <damiano.imola@outlook.it>
2024-02-21 14:55:20 +0100

    EUREKA! Did A* with only heuristics, I need to review a bit the DMAP to complete also the cost (i.e. g()) part. Actually A* is equal too Greedy Best First Search (GBFS) algorithm.

--- src/pose_handler/src/pose_handler_node.cpp
+++ src/pose_handler/src/pose_handler_node.cpp
@@ -24,17 +24,17 @@ nav_msgs::OccupancyGrid::ConstPtr global_map;
 struct Node
 {
     double x, y;
-    double cost;
+    double f; // f(n) = h(n) + g(n)
     Node* parent;
 
     // constructor as seen during lectures
-    Node(double x, double y, double cost, Node* parent = nullptr) : x(x), y(y), cost(cost), parent(parent) {}
+    Node(double x, double y, double f, Node* parent = nullptr) : x(x), y(y), f(f), parent(parent) {}
 };
 
 // function to compare between priority queues
 struct CompareNodes {
     bool operator()(const Node* a, const Node* b) const {
-        return a->cost > b->cost;
+        return a->f > b->f;
     }
 };
 
@@ -99,34 +99,118 @@ void gridToWorld(int grid_x, int grid_y, double& world_x, double& world_y)
 
 
 
-// heuristic function
-double h(){
 
+
+/*
+    #######################################
+    ####    ALGORITHM SIDE
+    #######################################
+*/
+
+// heuristic function
+double h(double curr_x, double curr_y, double goal_x, double goal_y){
+    return std::sqrt(std::pow(goal_x - curr_x, 2) + std::pow(goal_y - curr_y, 2));
 }
 
 // cost function
 double g(){
-
+    return 0.0;
 }
 
 
-// Dijkstra's algorithm considering cost based on the distance to the closest obstacle
-std::vector<geometry_msgs::PoseStamped> findPath(double start_x, double start_y, double goal_x, double goal_y)
+// A* algorithm
+std::vector<geometry_msgs::PoseStamped> a_star(double start_x, double start_y, double goal_x, double goal_y)
 {
-    if (!global_map)
-    {
+    if (!global_map) {
         ROS_ERROR("No map received");
         return std::vector<geometry_msgs::PoseStamped>();
     }
 
     int start_grid_x, start_grid_y, goal_grid_x, goal_grid_y;
 
+    // converting from World's coordinates to Grid's ones
     worldToGrid(start_x, start_y, start_grid_x, start_grid_y);
     worldToGrid(goal_x, goal_y, goal_grid_x, goal_grid_y);
 
     ROS_INFO("WORLD %lf, %lf, %lf, %lf", start_x, start_y, goal_x, goal_y);
     ROS_INFO("GRID %d, %d, %d, %d", start_grid_x, start_grid_y, goal_grid_x, goal_grid_y);
 
+
+    // priority queue allows us to discretize between elements order once push in list
+    std::priority_queue<Node*, std::vector<Node*>, CompareNodes> open_set;
+
+    // unordered_set is literally a set where we don't care about the order of nodes
+    std::unordered_set<Node*> closed_set;
+
+    // setting up the root of the A* tree
+    Node* start_node = new Node(start_grid_x, start_grid_y, 0);
+    open_set.push(start_node);
+
+    // iterating while there exists elements in open set
+    while (!open_set.empty())
+    {
+        Node* current = open_set.top();
+        open_set.pop();
+        ROS_INFO("### CURRENT %f %f, f(n) %f", current->x, current->y, current->f);
+
+        // adding root to the closed set
+        closed_set.insert(current);
+
+        // goal found
+        if (current->x == goal_grid_x && current->y == goal_grid_y)
+        {
+            std::vector<geometry_msgs::PoseStamped> path;
+            // converting back from grid to world 
+            while (current != nullptr)
+            {
+                geometry_msgs::PoseStamped pose;
+                gridToWorld(current->x, current->y, pose.pose.position.x, pose.pose.position.y);
+                path.push_back(pose);
+                current = current->parent;
+
+            }
+
+            // reversing the path (i.e. going from the start to the end)
+            std::reverse(path.begin(), path.end());
+            return path;
+        }
+
+        // iteration (assuming 8-connected tiles)
+        for (int dx = -1; dx <= 1; ++dx)
+        {
+            for (int dy = -1; dy <= 1; ++dy)
+            {
+                // if current node
+                if (dx == 0 && dy == 0) continue;
+
+                // computing next position to evaluate
+                int nx = current->x + dx;
+                int ny = current->y + dy;
+                ROS_INFO("%d %d %d %d", nx, ny, dx, dy);
+
+                // TODO: check if cell is within map boundaries
+
+                // ROS uses a vector representation of a matrix
+                int index = ny * global_map->info.width + nx;
+
+                // if we are in a "walkable" place
+                if (global_map->data[index] == 0)
+                {
+                    double heuristic = h(nx, ny, goal_grid_x, goal_grid_y);
+                    double cost = 0; //g();
+                    double evaluation_function = heuristic + cost;
+
+                    Node* neighbor = new Node(nx, ny, evaluation_function, current);
+
+                    if (closed_set.find(neighbor) == closed_set.end())
+                    {
+                        open_set.push(neighbor);
+                        closed_set.insert(neighbor);
+                    }
+                }
+            }
+        }
+    }
 
 
     // No path found
@@ -172,7 +256,7 @@ int main(int argc, char **argv)
 
     // run A* algorithm
     ROS_INFO("### SEARCHING PATH");
-    std::vector<geometry_msgs::PoseStamped> path = findPath(
+    std::vector<geometry_msgs::PoseStamped> path = a_star(
         global_initial_pose.position.x,
         global_initial_pose.position.y,
         global_goal_pose.position.x,
@@ -188,15 +272,24 @@ int main(int argc, char **argv)
     path_msg.header.frame_id = "map";
     path_msg.header.stamp = ros::Time::now();
 
+    // add poses to path path message
     for (const auto& pose : path)
     {
         path_msg.poses.push_back(pose);
     }
 
-    path_publisher.publish(path_msg);
-    ROS_INFO("### PATH PUBLISHED");
+    while (ros::ok())
+    {
+        // I've read that this is usefull
+        path_msg.header.stamp = ros::Time::now();
 
-    ros::spin();
+        // publish
+        path_publisher.publish(path_msg);
+        ros::spinOnce();
+
+        // publish at rate 1 Hz
+        ros::Rate(1).sleep();
+    }
 
     return 0;
 }
